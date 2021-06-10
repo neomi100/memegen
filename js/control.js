f22a4d8bf0f92d349ac5ab45dc6226e453fe6c8f
Neomi Goldberg <neomigoldberg@gmail.com>
2021-06-10 13:33:07 +0300

    lunch

--- js/control.js
+++ js/control.js
@@ -6,6 +6,9 @@ var gImgCtx;
 var gImg;
 var gAddCaption = 0
 
+var gTxtSize = 20
+
+
 
 init()
 function init() {
@@ -13,65 +16,97 @@ function init() {
     gCtx = gCanvas.getContext('2d');
     gImgCtx = gCanvas.getContext('2d');
     renderImags()
+
 }
 // רנדר גלריה
 function renderImags() {
-    var strImg = gImgs.map((img)=>{
-        //  gMeme.selectedImgId=img.id
-    return   `<img src="${img.src}"onclick="drawImgOnCanvas('${img.src}')"/>`  
-    //    return` <article><img src="${img.src}" onclick="drawImgOnCanvas('${img.src}')"></img></article>`
+    var strImg = gImgs.map((img) => {
+        // console.log(gMeme.selectedImgId);
+        gMeme.selectedImgId = img.id
+        return `<img src="${img.src}"onclick="drawImgOnCanvas('${img.src}')"/>`
+        //    return` <article><img src="${img.src}" onclick="drawImgOnCanvas('${img.src}')"></img></article>`
     });
-// console.log(gMeme.selectedImgId);
-    document.querySelector('.grid-container').innerHTML = strImg
+    // console.log(gMeme.selectedImgId);
+    document.querySelector('.grid-container').innerHTML = strImg.join('')
 }
 
-
+var gCurrSrc;
 
 function drawImgOnCanvas(src) {
     var img = new Image()
     img.src = src;
-    img.onload = function () {
+    img.onload = () => {
+
         gImgCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-    } 
-     pageEditor()
+    }
+    
+    gCurrSrc = img.src
+    pageEditor()
 }
-
-
-
 
 
 function drawText(text, x, y) {
-    if(gAddCaption>0){
-        y+=160
-        gMeme.lines.txt2=text
-        console.log('hi',);
+
+    if (gAddCaption === 1) {
+        y += 160
+        gMeme.lines[1].txt = text
+        console.log('hi', gMeme.lines[1].txt, gAddCaption);
     }
+    if (gAddCaption === 2) {
+        y = 130
+
+    }
+    // getIndex()
     gCtx.lineWidth = 2
     gCtx.strokeStyle = 'black'
     gCtx.fillStyle = 'white'
-    gCtx.font = '40px Arial'
+    gCtx.font = `${gTxtSize}px Arial`
     // gCtx.textAlign = 'center'
     gCtx.fillText(text, x, y)
-    console.log('hi');
     gCtx.strokeText(text, x, y)
     gCtx.save()
-    gAddCaption++
-    gMeme.selectedLineIdx=y
-    gMeme.lines.txt=text
-    gMeme.lines.size=gCtx.lineWidth
- 
+
+    gMeme.selectedLineIdx = y
+    gMeme.lines[0].txt = text
+    // console.log('hi',gMeme.lines.txt);
+    // gMeme.lines.size=gCtx.lineWidth
+    console.log(gMeme.lines[0].txt = text, 'first');
+
 }
 
- function addLine(){
-     document.getElementById('textInput1').value = '';
- }
- function deleteLine(){
-    gAddCaption=0
-    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
-    var filt= gMeme.lines.filter((line) =>{
-        console.log(filt, 'filt');
-        return  line.txt=true
-    })
+function fontSize() {
+
+}
+
+function minusSize(num) {
+    deleteLine()
+    gTxtSize += num
+    if(gTxtSize < 10) gTxtSize = 20
+    console.log(gTxtSize);
+}
+function plusSize(num) {
+    deleteLine()
+    gTxtSize += num
+    console.log(gTxtSize);
+
+}
+
+function addLine() {
+    document.getElementById('textInput1').value = '';
+    gAddCaption++
+    gMeme.selectedLineIdx = gAddCaption
+}
+function deleteLine() {
+    console.log(gCurrSrc);
+    // console.log(gImgs[gMeme.selectedImgId]);
+    drawImgOnCanvas(gCurrSrc)
+    // gAddCaption=0
+    // // console.log('hihi',gAddCaption);
+    // gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
+    // var filt= gMeme.lines.filter((line) =>{
+    //     console.log(filt, 'filt');
+    //     return  line.txt=true
+    // })
 }
 
 // PAGES
@@ -81,7 +116,9 @@ function pageHome() {
 }
 
 function pageEditor() {
+    document.getElementById('textInput1').value = '';
     document.querySelector('.editor-screen').style.display = 'block'
     document.querySelector('.home').style.display = 'none'
 }
+
 
--- js/service.js
+++ js/service.js
@@ -1,38 +1,72 @@
 'use strict'
 
-var gImgs = [{id: 1, src: 'images/1.jpg', keywords: ['happy'], isVisible:true},
-{id: 2, src: 'images/2.jpg', keywords: ['happy']},
-{id: 3, src: 'images/3.jpg', keywords: ['happy']},
-{id: 4, src: 'images/4.jpg', keywords: ['happy']},
-{id: 5, src: 'images/5.jpg', keywords: ['happy']},
-{id: 6, src: 'images/6.jpg', keywords: ['happy']},
-{id: 7, src: 'images/7.jpg', keywords: ['happy']},
-{id: 8, src: 'images/8.jpg', keywords: ['happy']},
-{id: 9, src: 'images/9.jpg', keywords: ['happy']},
-{id: 10, src: 'images/10.jpg', keywords: ['happy']},
-{id: 11, src: 'images/11.jpg', keywords: ['happy']},
-{id: 12, src: 'images/12.jpg', keywords: ['happy']},
-{id: 13, src: 'images/13.jpg', keywords: ['happy']},
-{id: 14, src: 'images/14.jpg', keywords: ['happy']},
-{id: 15, src: 'images/15.jpg', keywords: ['happy']},
-{id: 16, src: 'images/16.jpg', keywords: ['happy']},
-{id: 17, src: 'images/17.jpg', keywords: ['happy']},
-{id: 18, src: 'images/18.jpg', keywords: ['happy']},
+var gImgs = [{ id: 1, src: 'images/1.jpg', keywords: ['happy'], isVisible: true },
+{ id: 2, src: 'images/2.jpg', keywords: ['happy'] },
+{ id: 3, src: 'images/3.jpg', keywords: ['happy'] },
+{ id: 4, src: 'images/4.jpg', keywords: ['happy'] },
+{ id: 5, src: 'images/5.jpg', keywords: ['happy'] },
+{ id: 6, src: 'images/6.jpg', keywords: ['happy'] },
+{ id: 7, src: 'images/7.jpg', keywords: ['happy'] },
+{ id: 8, src: 'images/8.jpg', keywords: ['happy'] },
+{ id: 9, src: 'images/9.jpg', keywords: ['happy'] },
+{ id: 10, src: 'images/10.jpg', keywords: ['happy'] },
+{ id: 11, src: 'images/11.jpg', keywords: ['happy'] },
+{ id: 12, src: 'images/12.jpg', keywords: ['happy'] },
+{ id: 13, src: 'images/13.jpg', keywords: ['happy'] },
+{ id: 14, src: 'images/14.jpg', keywords: ['happy'] },
+{ id: 15, src: 'images/15.jpg', keywords: ['happy'] },
+{ id: 16, src: 'images/16.jpg', keywords: ['happy'] },
+{ id: 17, src: 'images/17.jpg', keywords: ['happy'] },
+{ id: 18, src: 'images/18.jpg', keywords: ['happy'] },
 ];
 
-var gKeywords = {'happy': 12,'funny puk': 1}
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
 
 
-var gMeme = { selectedImgId: 5, selectedLineIdx: 0,
-     lines: [ { txt:{
-         words:'I never eat Falafel',
-         isVisible:false
-     } ,txt2:{
-        words:'',
-        isVisible:false
-    }, size: 20, align: 'left', color: 'red' } ] }
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    // קאונטר
+    lines: [
+        {
+            txt: 'I never eat Falafel',
+            isVisible: true,
+            size: 2,
+            align: 'left',
+            color: 'red'
+        },
+        {
+            txt: '',
+            isVisible: true,
+            size: 2,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
 
 
-// function getImg(){
-//     return gImgs
+// const gMeme = {
+//     lines: [
+//         {
+//             text: 'daasd',
+//             size: 19
+//         },
+//         {
+//             text: 'text2',
+//                 size: 23
+//         }
+//     ]
 // }
+
+// how to use 
+
+// first item
+gMeme.lines[0].text
+gMeme.lines[0].size
+// second item
+gMeme.lines[1].text
+gMeme.lines[1].size
+
+
